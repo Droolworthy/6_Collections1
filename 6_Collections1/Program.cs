a2a74fdac8272406417d3d8e351ae6e83558a039
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2022-09-17 17:02:44 +0300

    Добавьте файлы проекта.

---
+++ 6_Collections1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6_Collections1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool canExitApp = true;
+            string userInput;
+            string meaningWord = "Изподвыподверта - это проверка на состояние алкогольного опьянения.";
+            string stopProgram = "Изподвыподверта";
+            List<string> words = new List<string>();
+            words.Add(stopProgram);
+
+            while (canExitApp)
+            {
+                Console.Write("Введите слово: ");
+                userInput = Console.ReadLine();
+
+                if (userInput == stopProgram)
+
+                    for (int i = 0; i < words.Count; i++)
+                    {
+                        Console.WriteLine(meaningWord);
+                        canExitApp = false;
+                    }
+                else
+                {
+                    Console.WriteLine("Такого слова нет в программе. Попробуйте ещё раз.");
+                }
+            }
+        }
+    }
+}
