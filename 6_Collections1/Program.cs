1c5ef90ae2ff77ca2e69671fcaf0ac9a2d22b776
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2022-09-17 17:21:29 +0300

    Update Program.cs

--- 6_Collections1/Program.cs
+++ 6_Collections1/Program.cs
@@ -9,27 +9,27 @@ namespace _6_Collections1
         {
             bool canExitApp = true;
             string userInput;
-            string meaningText = "Изподвыподверта - это проверка на состояние алкогольного опьянения.";
+            string meaningText = "Это проверка на состояние алкогольного опьянения.";
             string text = "Изподвыподверта";
             List<string> words = new List<string>();
             words.Add(text);
 
             while (canExitApp)
             {
-                Console.Write("Введите слово: ");
+                Console.Write("Ввод: ");
                 userInput = Console.ReadLine();
 
                 if (userInput == text)
                 {
                     for (int i = 0; i < words.Count; i++)
                     {
-                        Console.WriteLine(meaningText);
+                        Console.WriteLine(meaningText + " ");
                         canExitApp = false;
                     }
                 }
                 else
                 {
-                    Console.WriteLine("Такого слова нет в программе. Попробуйте ещё раз.");
+                    Console.WriteLine($"Такого слова {userInput} нет в программе. Попробуйте ещё раз.");
                 }
             }
         }
